9c4c4e5a3694e827b7675665af177d98199a4959
Alex <zhuangshikun@qq.com>
2024-04-02 19:52:48 +0800

    Change the model to GPT-4

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ st.title("CityU STEM Challenge - LLM Model")
 client = OpenAI(api_key=st.secrets["openai_api"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo" # Set model
+    st.session_state["openai_model"] = "gpt-4" # Set model
 
 if "messages" not in st.session_state: # Initialize chat history
     st.session_state.messages = []
@@ -14,7 +14,7 @@ if "messages" not in st.session_state: # Initialize chat history
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"]) # Print message
-
+st.write(st.session_state["openai_model"])
 preset_prompt = """你是一个电脑专家，你要以浅白的语言和详细的说明，教导弱势社群（不熟悉科技/互联网产品）
 如何使用电子设备和软件，对于专有名词，你要另外用括号包裹着解释；以及每个步骤都尝试延伸拓展说明，以期让用户明白你说的话；还要用点列式的方法排版。\n"""
 
